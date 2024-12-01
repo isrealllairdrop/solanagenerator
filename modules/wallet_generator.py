3527a01c260143ebad75c026e418a2deeb69218c
isrealllairdrop <167641753+isrealllairdrop@users.noreply.github.com>
2024-12-01 14:39:37 +0700

    Update wallet_generator.py

--- modules/wallet_generator.py
+++ modules/wallet_generator.py
@@ -1,15 +1,22 @@
-# wallet_generator/__init__.py
 from solana.keypair import Keypair
+from mnemonic import Mnemonic
+from file_handler import save_wallet_to_file
 
 def generate_keypair():
     """
-    Menghasilkan keypair baru untuk wallet Solana.
+    Menghasilkan keypair baru untuk wallet Solana dan mnemonic, serta menyimpan data ke dalam file.
     
     Returns:
-        tuple: Public key (base58), Private key (hexadecimal).
+        tuple: Public key (base58), Private key (hexadecimal), Mnemonic.
     """
     keypair = Keypair.generate()
     public_key = keypair.public_key.to_base58().decode()  # Public key dalam format base58
     private_key = keypair.secret_key.hex()  # Private key dalam format hexadecimal
+
+    # Menghasilkan mnemonic
+    mnemonic = Mnemonic("english").generate()  # Tidak perlu 'entropy' di sini
     
-    return public_key, private_key
+    # Simpan wallet dan mnemonic ke dalam file
+    save_wallet_to_file(public_key, private_key, mnemonic)
+
+    return public_key, private_key, mnemonic
