e833c41371f393e16b12ca59f59b7fd7b645ff83
isrealllairdrop <167641753+isrealllairdrop@users.noreply.github.com>
2024-12-01 14:39:08 +0700

    Update file_handler.py

--- modules/file_handler.py
+++ modules/file_handler.py
@@ -1,14 +1,17 @@
 # file_handler.py
-def save_wallet_to_file(public_key, private_key, filename="walletjadi.txt"):
+def save_wallet_to_file(public_key, private_key, mnemonic, filename="walletjadi.txt"):
     """
-    Menyimpan alamat (public key) dan kunci privat ke dalam file.
+    Menyimpan alamat (public key), kunci privat, dan mnemonic ke dalam file.
     
     Args:
         public_key (str): Alamat public key Solana dalam format base58.
         private_key (str): Kunci privat dalam format hexadecimal.
+        mnemonic (str): Mnemonic untuk wallet.
         filename (str): Nama file tempat menyimpan data wallet.
     """
     with open(filename, "w") as file:
         file.write(f"Alamat Solana: {public_key}\n")
         file.write(f"Private Key: {private_key}\n")
+        file.write(f"Mnemonic: {mnemonic}\n")
     print(f"Wallet berhasil disimpan ke {filename}")
+
